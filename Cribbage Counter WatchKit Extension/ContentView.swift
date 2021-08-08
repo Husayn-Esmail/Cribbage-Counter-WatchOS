6e29655f6d3d6614c0a8aa352d292e76c84b3d25
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2021-08-08 12:57:42 -0700

    Got counter working with reset screen popping up upon wins and boundaries for increases, good for full 121 point game.

--- Cribbage Counter WatchKit Extension/ContentView.swift
+++ Cribbage Counter WatchKit Extension/ContentView.swift
@@ -7,60 +7,135 @@
 
 import SwiftUI
 
+// Creating an observable object so that data can be passed around efficiently.
+class Counters: ObservableObject {
+    struct blueAndRed {
+        var blueScore: Int!
+        var redScore: Int!
+    }
+    @Published var scores = blueAndRed()
+    init() {
+        scores.blueScore = 0
+        scores.redScore = 0
+    }
+    static var shared = Counters()
+}
+
 struct ContentView: View {
-    @State var blueScore: Int = 0;
-    @State var redScore: Int = 0;
+    @ObservedObject var scores = Counters.shared;
+    @State var resetRequest: Bool = false;
+    
     var body: some View {
-        VStack {
-        HStack {
-            Text("Red")
-                .foregroundColor(.red)
-                .padding(20)
-            Text("Blue")
-                .padding(20)
-                .foregroundColor(.blue)
-        }
-            HStack {
-                Button(action: {if self.redScore != 0 {self.redScore -= 1}}, label: {
-            Text("-")
-                .font(.system(size: 23))
-        })
-        .background(Color.red)
-        .cornerRadius(20)
-                Button(action: {if self.redScore != 121 {self.redScore += 1}}, label: {
-            Text("+")
-                .font(.system(size: 23))
-        })
-        .background(Color.red)
-        .cornerRadius(20)
-                Button(action: {if self.blueScore != 0 {self.blueScore -= 1}}, label: {
-            Text("-")
-                .font(.system(size: 23))
-        })
-        .background(Color.blue)
-        .cornerRadius(20)
-                Button(action: {if self.blueScore != 121 {self.blueScore += 1}}, label: {
-            Text("+")
-                .font(.system(size: 23))
-        })
-        .background(Color.blue)
-        .cornerRadius(20)
-        }
-            HStack {
-                Text("\(blueScore)")
-                    .offset(x:54, y: 0)
-                Text("\(redScore)")
-                    .offset(x:-54, y: 0)
+        return Group  {
+            if scores.scores.redScore == 121 {
+                RedWinsView()
+            } else if scores.scores.blueScore == 121 {
+                BlueWinsView()
+            } else {
+                VStack {
+                    HStack {
+                        // display score labels
+                        Text("Red")
+                            .foregroundColor(.red)
+                            .padding(20)
+                        Text("Blue")
+                            .padding(20)
+                            .foregroundColor(.blue)
+                    }
+                    HStack {
+                        // decrease score for red
+                        Button(action: {if scores.scores.redScore != 0 {scores.scores.redScore -= 1}}, label: {
+                            Text("-")
+                                .font(.system(size: 23))
+                        })
+                        .background(Color.red)
+                        .cornerRadius(20)
+                        // increase score for red
+                        Button(action: {if scores.scores.redScore != 121 {scores.scores.redScore += 1}}, label: {
+                            Text("+")
+                                .font(.system(size: 23))
+                        })
+                        .background(Color.red)
+                        .cornerRadius(20)
+                            // decrease score for blue
+                        Button(action: {if scores.scores.blueScore != 0 {scores.scores.blueScore -= 1}}, label: {
+                            Text("-")
+                                .font(.system(size: 23))
+                        })
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        // increase score for blue
+                        Button(action: {if scores.scores.blueScore != 121 {scores.scores.blueScore += 1}}, label: {
+                            Text("+")
+                                .font(.system(size: 23))
+                        })
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                    }
+                    // display scores
+                    HStack {
+                        Text("\(scores.scores.blueScore)")
+                            .offset(x:54, y: 0)
+                        Text("\(scores.scores.redScore)")
+                            .offset(x:-54, y: 0)
+                    }
+                
+                }
             }
-            Button(action:{self.blueScore = 0; self.redScore = 0;}, label: {Text("Reset")
-            })
-            .background(Color.gray)
-            .cornerRadius(10)
         }
 // I want to build a winner screen and a separate screen for reset so that it's not accidentally tapped. perhaps reset can be on a different page. I could also create a screen to select a 61 point game or a 121 point game.
         
 }
+ 
+// if red wins
+struct RedWinsView: View {
+    @ObservedObject var scores = Counters.shared
+    var body: some View {
+        VStack {
+            Text("Red Wins!")
+                .foregroundColor(.red)
+            Text("\(scores.scores.redScore)")
+            Button(action:{scores.scores.blueScore = 0; scores.scores.redScore = 0;}, label: {Text("Reset")
+            })
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
+    }
+}
+    
+// if blue wins
+struct BlueWinsView: View{
+    @ObservedObject var scores = Counters.shared
+    var body: some View {
+        VStack {
+        Text("Blue Wins!")
+            .foregroundColor(.blue)
+            Text("\(scores.scores.blueScore)")
+        Button(action:{scores.scores.blueScore = 0; scores.scores.redScore = 0;}, label: {Text("Reset")
+        })
+        .background(Color.gray)
+        .cornerRadius(10)
+        }
+    }
+}
+    
+// Reset confirmation view for ContentView screen
+struct ResetView: View {
+    var body: some View {
+        Text("Are you sure you want to reset?")
+    }
+}
 
+//struct SixtyOnePointView: View {
+//    var body: some View {
+//        Text("61 point view")
+//    }
+//}
+    
+    
+
+    
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
